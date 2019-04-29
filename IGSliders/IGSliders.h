24774d0324a401a912992f9c6ff31d1166e33dbb
Łukasz Dziedzic <dziedzic@alfabety.pl>
2019-04-29 10:01:43 +0200

    Initial Commit

---
+++ IGSliders/IGSliders.h
@@ -0,0 +1,19 @@
+//
+//  IGSliders.h
+//  IGSliders
+//
+//  Created by Łukasz Dziedzic on 29/04/2019.
+//  Copyright © 2019 Łukasz Dziedzic. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for IGSliders.
+FOUNDATION_EXPORT double IGSlidersVersionNumber;
+
+//! Project version string for IGSliders.
+FOUNDATION_EXPORT const unsigned char IGSlidersVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <IGSliders/PublicHeader.h>
+
+
